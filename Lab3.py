b1d5eb85c5b7ed118d59150f9ad048849125a32e
ri0452384 <evolnoob@gmail.com>
2017-10-18 23:37:12 +0800

    fixed UI, added extra columns

--- Lab3.py
+++ Lab3.py
@@ -1,13 +1,7 @@
 """
-CMSC 125 Lab3 v 1.0 by Rayven N. Ingles
+CMSC 125 Lab3: Malloc simulator v 1.0 by Rayven N. Ingles
    All wrongs reversed 2017
-"""
-from tkinter import *
-from tkinter import filedialog
-from tkinter import messagebox
-"""
-problem definition:
-
+--------------------------------------------------------------------------------
 JOB LIST
 Job Stream #    Time    Job Size
 1                  5       5760
@@ -35,7 +29,6 @@ Job Stream #    Time    Job Size
 23                 8       8390
 24                 5       5950
 25                10        760
-
 MEMORY LIST
 Memory Block Size
 1       9500
@@ -48,7 +41,6 @@ Memory Block Size
 8       5500
 9       1500
 10       500
-
 At one large batch-processing computer installation, the management wants to decide what storage
 placement strategy will yield the best possible performance. The installation runs a large real storage
 computer under fixed partition multiprogramming. Each user program runs in a single group of
@@ -76,20 +68,23 @@ about the performance of the system for this job mix and memory organization. Is
 of partitioning better than the other? Why or why not? Could you recommend one method over
 the other based on your sample run? Would this hold in all cases? Write some conclusions and
 recommendations.
-
 """
+from tkinter import *
+from tkinter import messagebox
+
 class Memblock:
-    id=0
     size=0
+    times_used=0
     current_job=None
-    is_free=True
+    in_use=False
+    largest_fragmentation=0
     def __init__(self,idNumber,size):
         self.id = idNumber
         self.size = size
         self.current_job=None
 
     def __repr__(self):
-        return "Mem block %s: %sKB" % (self.id,self.size)
+        return "Mem %s: %sKB remaining" % (self.id,self.size)
 
     def __copy__(self):
         return Memblock(self.id,self.size)
@@ -109,8 +104,8 @@ class Job:
 
 def initialize_jobs():
     global waiting
-    for j in waiting:
-        j.__init__()
+    # for j in waiting:
+    #     j.__init__()
     job1 = Job(1, 5, 5760)
     job2 = Job(2, 4, 4190)
     job3 = Job(3, 8, 3290)
@@ -142,7 +137,8 @@ def initialize_jobs():
 #initialize globals
 waiting =[]
 memories=[]
-t = 0
+done = []
+t = -1
 mem1= Memblock(1, 9500)
 mem2= Memblock(2, 7000)
 mem3= Memblock(3, 4500)
@@ -154,78 +150,119 @@ mem8= Memblock(8, 5500)
 mem9= Memblock(9, 1500)
 mem10= Memblock(10,500)
 memories = mem1,mem2,mem3,mem4,mem5,mem6,mem7,mem8,mem9,mem10
-freelist = [Memblock]
-initialize_jobs()
 started=FALSE
+timer_id = None
+total_internal_fragmentation=0
 
 #text for the Help menu
 def showhelp():
-    messagebox.showinfo('help file goes here')
+    messagebox.showinfo(title="Malloc Simulator Help",message='Select the desired algorithm between First-Fit, Best-Fit, or Worst-Fit. Then, it will simulate memory allocation of the preset jobs.')
 
 def clock():
-    global started
-    global freelist
     global waiting
     global memories
     global t
-
-
-    #all memory slots are free during start
-    for m in memories:
-        freelist.append(m.__copy__())
-    r=len(waiting)-1
-    s=len(freelist) -1
-    is_empty = False
-    while(len(waiting) > 0 and is_empty == False):
-        for i in range(0,r):
-            for j in range(0,s):
-                if(waiting[i].size < freelist[j].size and freelist[j].is_free):
-                    print(freelist[j],waiting[i])
-                    allocate = waiting.pop(i)
-                    r-=1
-                    freelist[j].current_job = allocate
-                    freelist[j].size -= freelist[j].current_job.size
-                    freelist[j].is_free = False
-                    print(allocate)
-                    allocate = None
-                if(j >=9):
-                    j=0;
-
+    global timer_id
+    global done
+    global total_internal_fragmentation
+    usecounter = 0
+    total_internal_fragmentation = 0
+    #start of segment to update all labels of each memory locations and their current jobs
+    for i in range(0,len(memories)):
+        memlabels[i].config(text=memories[i].__repr__())
+        joblabels[i].config(text=memories[i].current_job.__repr__())
+        countlabels[i].config(text="%s\t|\t\t%s" % (memories[i].times_used,memories[i].largest_fragmentation))
+        total_internal_fragmentation +=memories[i].largest_fragmentation
+    wait_text = ""
+    for i in waiting:
+        wait_text += i.__repr__()+"\n"
+    waitlist.config(text=wait_text)
+    done_text=""
+    for p in done:
+        done_text += "Job %s, wait: %sms \n" % (p.id.__repr__(),(p.wtime+1).__repr__())
+    jobs_done.config(text=done_text)
+    total_fragmentation.config(text="Total:\t\t%s"%total_internal_fragmentation)
+    #end of label update segment
+    for mem in memories:
+        if mem.current_job !=None:
+            mem.current_job.time -=1
+            #if current job has completed execution, frees up the mem slot
+            if mem.current_job.time ==0:
+                mem.size += mem.current_job.size
+                done.append(mem.current_job)
+                mem.current_job = None
+                mem.times_used +=1
+                mem.in_use = False
+        if mem.in_use:
+            usecounter +=1
+        if usecounter <= 10:
+            for j in waiting:
+                if j.size < mem.size and mem.in_use==False:
+                    #print("allocating",j," to ",mem)
+                    j.wtime = t
+                    mem.current_job=j
+                    mem.size -= j.size
+                    mem.largest_fragmentation = max(mem.size,mem.largest_fragmentation)
+                    waiting.remove(j)
+                    mem.in_use = True
     t+=1
-    timelabel.config(text=t,font='times 25')
-    for f in freelist:
-        if f.is_free != False:
-            f.current_job.time -= 1
-        if f.current_job.time <=0:
-            print('*')
-            f.current_job=None    #run to completion, memory is then returned to the free list
-            f.is_free = True
-
-    for j in range(0,len(waiting)):
-        waiting[j].wtime += 1
-    root.after(1000,clock)
+    timelabel.config(text="%sms"%t,font='times 25')
+    timer_id = root.after(1000,clock)
 
 def simulate_first_fit():
-    started = TRUE
+    global memories
+    if timer_id != None:
+       root.after_cancel(timer_id)
+    initialize_jobs()
+    memories = sorted(memories,key=lambda x:x.id,reverse=False)
+    for m in memories:
+        m.times_used = 0
+        m.largest_fragmentation=0
+    global t
+    t= -1
+    global done
+    done = []
+    fitlabel.config(text="First Fit")
     clock()
 
 def simulate_best_fit():
-    global started
+    if timer_id != None:
+       root.after_cancel(timer_id)
+    initialize_jobs()
+    global memories
+    global t
+    t= -1
+    global done
+    done = []
+    fitlabel.config(text="Best Fit")
+    #sort the mem partitions in ascending order
+    memories = sorted(memories,key=lambda x:x.size,reverse=False)
+    for m in memories:
+        m.times_used = 0
+        m.largest_fragmentation=0
     clock()
-    print(t)
-    started = TRUE
 
 def simulate_worst_fit():
-    global started
+    if timer_id != None:
+       root.after_cancel(timer_id)
+    initialize_jobs()
+    global memories
+    global t
+    t= -1
+    global done
+    done = []
+    fitlabel.config(text="Worst Fit")
+    #sort the mem partitions in descending order
+    memories = sorted(memories,key=lambda x:x.size,reverse=True)
+    for m in memories:
+        m.times_used = 0
+        m.largest_fragmentation=0
     clock()
-    print(t)
-    started = TRUE
 
 """
 main window code goes  here
 """
 root = Tk()
-started = FALSE
 root.minsize(width=325, height=100)
 
 #left frame here
@@ -233,37 +270,107 @@ mainframe = Frame(root, bg='#2B2B2B')
 mainframe.pack()
 
 menu = Menu(root, tearoff=False)
-root.title("Lab 3 v1.0")
+root.title("Memory Allocation simulator v1.0")
 root.config(menu=menu)
 
 #labels here:
-
+#initialize the label that shows the timer
 timelabel=Label(mainframe,justify='left')
 timelabel.grid(row=0,column=0)
 timelabel.config(text = "not started")
-
-
+#label that shows either "First Fit", "Best Fit", or "Worst Fit"
+fitlabel = Label(mainframe,justify='left', text= "Click File > Help for instructions",font='times 15')
+fitlabel.grid(row=0,column=1)
+#label for mem1
 label1 = Label(mainframe, text=mem1.__repr__())
 label1.grid(row=1,column=0)
+job1 = Label(mainframe,text="None")
+job1.grid(row=1,column=1)
+count1 = Label(mainframe,text=mem1.times_used)
+count1.grid(row=1,column=2)
+#label for mem2
 label2 = Label(mainframe, text=mem2.__repr__())
 label2.grid(row=2,column=0)
+job2 = Label(mainframe,text="None")
+job2.grid(row=2,column=1)
+count2 = Label(mainframe,text=mem1.times_used)
+count2.grid(row=2,column=2)
+#label for mem3
 label3 = Label(mainframe, text=mem3.__repr__())
 label3.grid(row=3,column=0)
+job3 = Label(mainframe,text="None")
+job3.grid(row=3,column=1)
+count3 = Label(mainframe,text=mem1.times_used)
+count3.grid(row=3,column=2)
+#label for mem4
 label4 = Label(mainframe, text=mem4.__repr__())
 label4.grid(row=4,column=0)
+job4 = Label(mainframe,text="None")
+job4.grid(row=4,column=1)
+count4 = Label(mainframe,text=mem1.times_used)
+count4.grid(row=4,column=2)
+#label for mem5
 label5 = Label(mainframe, text=mem5.__repr__())
 label5.grid(row=5,column=0)
+job5 = Label(mainframe,text="None")
+job5.grid(row=5,column=1)
+count5 = Label(mainframe,text=mem1.times_used)
+count5.grid(row=5,column=2)
+#label for mem6
 label6 = Label(mainframe, text=mem6.__repr__())
 label6.grid(row=6,column=0)
+job6 = Label(mainframe,text="None")
+job6.grid(row=6,column=1)
+count6 = Label(mainframe,text=mem1.times_used)
+count6.grid(row=6,column=2)
+#label for mem7
 label7 = Label(mainframe, text=mem7.__repr__())
 label7.grid(row=7,column=0)
+job7 = Label(mainframe,text="None")
+job7.grid(row=7,column=1)
+count7 = Label(mainframe,text=mem1.times_used)
+count7.grid(row=7,column=2)
+#label for mem8
 label8 = Label(mainframe, text=mem8.__repr__())
 label8.grid(row=8,column=0)
+job8 = Label(mainframe,text="None")
+job8.grid(row=8,column=1)
+count8 = Label(mainframe,text=mem1.times_used)
+count8.grid(row=8,column=2)
+#label for mem9
 label9 = Label(mainframe, text=mem9.__repr__())
 label9.grid(row=9,column=0)
+job9 = Label(mainframe,text="None")
+job9.grid(row=9,column=1)
+count9 = Label(mainframe,text=mem1.times_used)
+count9.grid(row=9,column=2)
+#label for mem10
 label10 = Label(mainframe, text=mem10.__repr__())
 label10.grid(row=10,column=0)
-
+job10 = Label(mainframe,text="None")
+job10.grid(row=10,column=1)
+count10 = Label(mainframe,text=mem1.times_used)
+count10.grid(row=10,column=2)
+"""place all of the labels on a list for easier update"""
+countlabels=[count1,count2,count3,count4,count5,count6,count7,count8,count9,count10]
+memlabels = [label1,label2,label3,label4,label5,label6,label7,label8,label9,label10]
+joblabels = [job1,job2,job3,job4,job5,job6,job7,job8,job9,job10]
+"""column header for the count and largest fragmentation"""
+uselabel=Label(mainframe,text="# of times used | largest internal fragmentation")
+uselabel.grid(row=0,column=2)
+"""label that shows all the jobs which are waiting"""
+waitlabel = Label(mainframe,text="Jobs not allocated:")
+waitlabel.grid(row=11,column=0)
+waitlist = Label(mainframe,text=waiting,justify='left')
+waitlist.grid(row=12,column=0)
+"""label that shows all jobs done"""
+job_done_label = Label(mainframe,text="Jobs Completed: ")
+job_done_label.grid(row=11,column=1)
+jobs_done = Label(mainframe,text="None")
+jobs_done.grid(row=12,column=1)
+"""label that shows the total for largest internal fragmentation"""
+total_fragmentation = Label(mainframe,text = "Total:\t\t%s"%total_internal_fragmentation)
+total_fragmentation.grid(row=11,column=2)
 """
 submenu code goes here
 """
