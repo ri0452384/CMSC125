decfa1381237170a17fbf31e192ef306d9f332d7
ri0452384 <rayven.ingles@gmail.com>
2019-05-08 21:07:56 +0800

    Update MP4-2.py

--- MP4-2.py
+++ MP4-2.py
@@ -1,9 +1,10 @@
 """
-This code will recognize the input string as either infix, prefix, or postfix, and generate its other counterparts from the given input
+This code will convert the input string into its postfix equivalent to evaluate its value. returns an integer value only.
 
 references:
 http://interactivepython.org/lpomz/courselib/static/pythonds/BasicDS/InfixPrefixandPostfixExpressions.html
 https://www.aoprogrammer.com/infix-prefix-and-postfix-notation/
+by: Rayven N. Ingles
 
 """
 
@@ -12,9 +13,9 @@ from pythonds.basic.stack import Stack
 
 def classify(expression):
     tokenList = expression.split()
-    if tokenList[0] in "^*/+-":
+    if tokenList[0] in "^*/%+-":
         return "prefix"
-    elif tokenList[-1] in "^*/+-":
+    elif tokenList[-1] in "^*/%+-":
         return "postfix"
     else:
         return "infix"
@@ -25,12 +26,13 @@ def prefixToInfix(prefixexpr):
     prec["^"] = 4
     prec["*"] = 3
     prec["/"] = 3
+    prec["%"] = 3
     prec["+"] = 2
     prec["-"] = 2
     infixStack = Stack()
     tokenList = prefixexpr[::-1].split()
     for token in tokenList:
-        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" or token in "0123456789":
+        if token.isdigit():
             infixStack.push(token.__str__())
         else:
             operand1 = infixStack.pop()
@@ -44,71 +46,13 @@ def prefixToInfix(prefixexpr):
         print("Error! Invalid Input!")
         exit(1)
 
-
-def infix_to_prefix(infixexpr):
-    prec = {}
-    prec["^"] = 4
-    prec["*"] = 3
-    prec["/"] = 3
-    prec["+"] = 2
-    prec["-"] = 2
-    prec[")"] = 1
-    prec["("] = 1
-    opStack = Stack()
-    prefixStack = Stack()
-    tokenList = infixexpr[::-1].split()
-    for token in tokenList:
-        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" or token in "0123456789":
-            prefixStack.push(token)
-        elif token == ')':
-                opStack.push(token)
-        elif token == '(':
-            topToken = opStack.pop()
-            while topToken != ')':
-                prefixStack.push(topToken)
-                topToken = opStack.pop()
-        else:
-            while (not opStack.isEmpty()) and (prec[opStack.peek()] >= prec[token]):
-                prefixStack.push(opStack.pop())
-            opStack.push(token)
-
-    while not opStack.isEmpty():
-        prefixStack.push(opStack.pop())
-    output = ""
-    while not prefixStack.isEmpty():
-        output += prefixStack.pop()+ " "
-    return output
-
-
-def postfix_to_infix(postfixexpr):
-    infixStack = Stack()
-    tokenList = postfixexpr.split()
-    print(tokenList)
-    for token in tokenList:
-        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" or token in "0123456789":
-            infixStack.push(token)
-        else:
-            if infixStack.size() < 2:
-                print("Error! Invalid Input")
-                exit(-1)
-            else:
-                operand2 = infixStack.pop()
-                operand1 = infixStack.pop()
-                infixStack.push(("( " + operand1 + " " + token + " " + operand2 + " )"))
-    output = ""
-    output += infixStack.pop()
-    if infixStack.isEmpty():
-        return output
-    else:
-        print("Error! Invalid Input!")
-        exit(1)
-
-
+        
 def infix_to_postfix(infixexpr):
     prec = {}
     prec["^"] = 4
     prec["*"] = 3
     prec["/"] = 3
+    prec["%"] = 3
     prec["+"] = 2
     prec["-"] = 2
     prec["("] = 1
@@ -117,7 +61,7 @@ def infix_to_postfix(infixexpr):
     tokenList = infixexpr.split()
 
     for token in tokenList:
-        if token in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" or token in "0123456789":
+        if token.isdigit():
             postfixList.append(token)
         elif token == '(':
             opStack.push(token)
@@ -127,8 +71,7 @@ def infix_to_postfix(infixexpr):
                 postfixList.append(topToken)
                 topToken = opStack.pop()
         else:
-            while (not opStack.isEmpty()) and \
-               (prec[opStack.peek()] >= prec[token]):
+            while (not opStack.isEmpty()) and (prec[opStack.peek()] >= prec[token]):
                   postfixList.append(opStack.pop())
             opStack.push(token)
 
@@ -142,7 +85,7 @@ def postfix_eval(postfixExpr):
     tokenList = postfixExpr.split()
 
     for token in tokenList:
-        if token in "0123456789":
+        if token.isdigit():
             operandStack.push(int(token))
         else:
             operand2 = operandStack.pop()
@@ -153,15 +96,29 @@ def postfix_eval(postfixExpr):
 
 def do_math(op, op1, op2):
     if op == "^":
-        return op1 ^ op2
+        ans = op1 ** op2
+        print(op1,op,op2,ans)
+        return ans
     elif op == "*":
-        return op1 * op2
+        ans = op1 * op2
+        print(op1,op,op2,ans)
+        return ans
     elif op == "/":
-        return op1 / op2
+        ans = int(float(op1 / op2))
+        print(op1,op,op2,ans)
+        return ans
     elif op == "+":
-        return op1 + op2
+        ans = op1 + op2
+        print(op1,op,op2,ans)
+        return ans
+    elif op == "%":
+        ans = op1 % op2
+        print(op1,op,op2,ans)
+        return ans
     else:
-        return op1 - op2
+        ans = op1 - op2
+        print(op1,op,op2,ans)
+        return ans
 
 
 #main starts here
@@ -170,16 +127,13 @@ classification = classify(entered_input)
 
 if classification == "infix":
     print("Infix detected \n infix:"+entered_input)
-    print("\n prefix: "+infix_to_prefix(entered_input))
-    print("\n postfix: "+infix_to_postfix(entered_input))
+    print("\n value: "+postfix_eval(infix_to_postfix(entered_input)).__str__())
 elif classification == "postfix":
     print("Postfix detected \n postfix: " + entered_input)
-    a = postfix_to_infix(entered_input)
-    print("\n infix: "+a)
-    print("\n prefix: "+infix_to_prefix(a))
+    print("\n value: "+postfix_eval(entered_input).__str__())
 else:
     print("Prefix detected \n prefix: " + entered_input)
     a = prefixToInfix(entered_input)
     print("\n infix: "+a)
-    print("\n postfix: "+infix_to_postfix(a))
+    print("\n value: "+postfix_eval(infix_to_postfix(a)).__str__())
 
